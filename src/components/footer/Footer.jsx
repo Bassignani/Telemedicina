8c39a547c781d25b702ce3374250087c3710dce3
Matias Barril <matibarril@gmail.com>
2024-05-13 23:03:55 -0300

    Home page_ firsts steps

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import "./footer.css";
+import Logo from '@logos/Logo_TM1.png';
 
 const Footer = () => {
   return (
     <footer>
-      <p>Pie de página</p>
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="center">
+        <p >By LimayDev</p>
+       
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "@styles/global.css";
+import "./home.css";
+import Banner from '@images/banner-medico.png';
+
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>Hola Mundo</p>
+      {/* Sección superior */}
+      <div className="top-section">
+        {/* Columna izquierda */}
+        <div className="left-column">
+          <p>Conectando a pacientes con médicos, sin importar la distancia, a través de la telemedicina.</p>
+          <p>Tu salud en línea, siempre al alcance de tu mano.</p>
+        </div>
+        {/* Columna derecha */}
+        <div className="right-column">
+          <img src={Banner} alt="Banner" />
+        </div>
+      </div>
+      {/* Sección inferior */}
+      <div className="bottom-section">
+        <p>Conectamos pacientes con médicos.</p>
+      </div>
     </div>
   );
 }
